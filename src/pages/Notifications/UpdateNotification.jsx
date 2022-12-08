64ac17f52c29a10196022688b1a14a52923ada95
JoinerTim <nguyendinhnghiaqwerty@gmail.com>
2022-12-08 09:06:11 +0700

    my commit

--- src/pages/Notifications/UpdateNotification.jsx
+++ src/pages/Notifications/UpdateNotification.jsx
@@ -226,7 +226,7 @@ const UpdateNotification = ({
     try {
       await axios.put(
         `http://18.140.66.234/api/v1/notifications?${facultyUrl}${notifyGCUrl}${classCodesUrl}type=${
-          typeSelected.value ? 0 : typeSelected.value
+          typeSelected.value === undefined ? 0 : typeSelected.value
         }&` +
           new URLSearchParams({
             content: draftToHtml(convertToRaw(editorState.getCurrentContent())),
